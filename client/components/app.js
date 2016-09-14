e791c1d1e22493c55edf0cdbbe50468fc32bdc60
Chris Ritchie <chrisritchi@gmail.com>
2016-09-14 16:32:40 -0500

    adding music section and code section

--- client/components/app.js
+++ client/components/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Music from './music'
+import Code from './code'
+
 
 class App extends React.Component {
 	render(){
 		return (
 			<div>
 				<h1>Chris Ritchie is so cool.com</h1>
+				<Music />
+				<Code />
 			</div>
 		)
 	}
---
+++ client/components/code.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Code extends Component{
+constructor(props){
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render(){
+
+  	return(
+  		<div> Code Stuff </div>
+  		)
+  }
+}
